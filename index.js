b791707683674c2cbe136942fc8d0d0c49a53ecc
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-09 17:31:13 +0600

    routes created

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRouter from "./routes/auth.js";
+import usersRouter from "./routes/users.js";
+import hotelsRouter from "./routes/hotels.js";
+import roomsRouter from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -12,6 +16,12 @@ const connect = async () => {
     throw error;
   }
 };
+
+const api = process.env.API;
+app.use(`${api}/auth`, authRouter);
+app.use(`${api}/users`, usersRouter);
+app.use(`${api}/hotels`, hotelsRouter);
+app.use(`${api}/rooms`, roomsRouter);
 
 mongoose.connection.on("disconnected", () =>
   console.log("mongoDB disconnected")
