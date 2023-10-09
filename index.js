d5d3b7cc42d587cf06784dd54c3c9559e05ae7e1
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-09 17:15:26 +0600

    DB connected

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () =>
+  console.log("mongoDB disconnected")
+);
+mongoose.connection.on("connected", () => console.log("mongoDB connected"));
+
+app.listen(process.env.PORT, () => {
+  connect();
+  console.log(`Listening on Port ${process.env.PORT}`);
+});
